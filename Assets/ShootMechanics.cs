aefc4a8bc8011be778c7589d642f0079c5889818
Sgoel-exe <sgoel@email.arizona.edu>
2023-06-22 15:48:52 -0700

    hold down to shoot works now

--- Assets/ShootMechanics.cs
+++ Assets/ShootMechanics.cs
@@ -23,6 +23,9 @@ public class ShootMechanics : MonoBehaviour
     [SerializeField] private int reservedAmmo = 24;
     public InputAction reloadKey;
     public Text ammotext;
+
+    [SerializeField] private float fireRate = 0.2f;
+    private float timer = 0f;
     private void Awake()
     {
         playerInput = new PlayerMovements();
@@ -45,7 +48,12 @@ public class ShootMechanics : MonoBehaviour
         reloadKey.Disable();
     }
 
-    void Fire(InputAction.CallbackContext context)
+    void Fire(InputAction.CallbackContext callbackContext)
+    {
+        fire(false);
+    }
+
+    void fire(bool isHeld)
     {
         if (canShoot())
         {
@@ -54,6 +62,7 @@ public class ShootMechanics : MonoBehaviour
             Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
             //Instantiate(MuzzleFlash, firePoint.position, Quaternion.Euler(0,0,0));
             Invoke("StopShoot", animationTime);
+            
         }
         else
         {
@@ -68,6 +77,7 @@ public class ShootMechanics : MonoBehaviour
 
     public IEnumerator reload()
     {
+        //Invoke("StopShoot", 0.01f);
         fireAction.Disable();
         yield return new WaitForSeconds(reloadTime);
         fireAction.Enable();
@@ -95,7 +105,7 @@ public class ShootMechanics : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     public void addAmmo(int amount)
@@ -124,6 +134,23 @@ public class ShootMechanics : MonoBehaviour
     private void LateUpdate()
     {
         ammotext.text = (currentBullets.ToString() + "/" + reservedAmmo.ToString());
+    }
+
+    private void FixedUpdate()
+    {
+        if (fireAction.ReadValue<float>() > 0.1f)
+        {
+            if(timer >= fireRate)
+            {
+                timer = 0;
+                fire(true);
+            }
+            else
+            {
+                timer += Time.smoothDeltaTime;
+            }
+        }
+
     }
 
     private void StopShoot()
